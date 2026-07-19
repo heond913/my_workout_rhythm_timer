3e0479544d42d193ac464802fd2b4e909060466a
heond913 <63638406+heond913@users.noreply.github.com>
2026-07-19 14:56:33 +0900

    build: update version name to 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 5
-    versionName = "5.0"
+    versionName = "3.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
